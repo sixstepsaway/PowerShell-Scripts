42a09d181374c9a809b82ddd1c3e04062b712211
Jess <61681613+sixstepsaway@users.noreply.github.com>
2022-12-29 03:00:30 +0000

    Update FFMPEGProcessTV.ps1

--- FFMPEGProcessTV.ps1
+++ FFMPEGProcessTV.ps1
@@ -1,70 +1,3 @@
-
-<#foreach ($inputVid in $originalVids) {
-    $outputVid = [io.path]::ChangeExtension($inputVid.FullName, '.mp4')
-    ffmpeg.exe -i $inputVid.FullName -c:v libx264 -crf 18 -c:a aac -map_metadata 0 $outputVid
-}#>
-
-
-Function Start-RestrictedStandard {
-    $videos = Get-ChildItem "$location\*.mkv"
-    foreach ($video in $videos) {
-        ffmpeg -threads 2 -i $video -map 0:a:0 "$location\Converted\$($video.BaseName).mp3" -map 0:a:1 "$location\Converted\$($video.BaseName).mp3" -map 0:a:2 "$location\Converted\$($video.BaseName).mp3" -map 0:a:3 "$location\Converted\$($video.BaseName).mp3" -map 0:a:4 "$location\Converted\$($video.BaseName).mp3" -map 0:a:5 "$location\Converted\$($video.BaseName).mp3"
-	    ffmpeg -threads 2 -i $video $dolby2 "$location\Converted\$($video.BaseName).mp4"
-	    ffmpeg -threads 2 -i $video -c:v libx264 -profile:v baseline -crf 16  -pix_fmt yuv420p -vf "$dolby,scale=640:360" -an "$location\Converted\Proxy\$($video.BaseName).mp4" 
-    }
-}
-Function Start-UnlimitedStandard {
-    $videos = Get-ChildItem "$location\*.mkv"
-    foreach ($video in $videos) {
-        ffmpeg -i $video -map 0:a:0 "$location\Converted\$($video.BaseName).mp3" -map 0:a:1 "$location\Converted\$($video.BaseName).mp3" -map 0:a:2 "$location\Converted\$($video.BaseName).mp3" -map 0:a:3 "$location\Converted\$($video.BaseName).mp3" -map 0:a:4 "$location\Converted\$($video.BaseName).mp3" -map 0:a:5 "$location\Converted\$($video.BaseName).mp3"
-	    ffmpeg -i $video $dolby2 "$location\Converted\$($video.BaseName).mp4"
-	    ffmpeg -i $video -c:v libx264 -profile:v baseline -crf 16  -pix_fmt yuv420p -vf "$dolby,scale=640:360" -an "$location\Converted\Proxy\$($video.BaseName)_PROXY.mp4" 
-    }
-}
-
-Function Start-RestrictedComplex {
-    $videos = Get-ChildItem "$location\*.mkv"
-    foreach ($video in $videos) {
-        ffmpeg -threads 2 -i "$video" -filter_complex "channelsplit=channel_layout=5.1[$letterMap01][$letterMap02][$letterMap03][$letterMap04][$letterMap05][$letterMap06]"	-map "[$letterMap01]" "$location\Converted\$($video.BaseName)___TRACK01.mp3" -map "[$letterMap02]" "$location\Converted\$($video.BaseName)___TRACK02.mp3" -map "[$letterMap03]" "$location\Converted\$($video.BaseName)___DIALOGUE.mp3" -map "[$letterMap04]" "$location\Converted\$($video.BaseName)___TRACK04.mp3" -map "[$letterMap05]" "$location\Converted\$($video.BaseName)___TRACK05.mp3" -map "[$letterMap06]" "$location\Converted\$($video.BaseName)___TRACK06.mp3"
-	    ffmpeg -threads 2 -i $video $dolby2 "$location\Converted\$($video.BaseName).mp4"
-	    ffmpeg -threads 2 -i $video -c:v libx264 -profile:v baseline -crf 16  -pix_fmt yuv420p -vf "$dolby,scale=640:360" -an "$location\Converted\Proxy\$($video.BaseName)_PROXY.mp4" 
-    }
-}
-
-Function Start-UnlimitedComplex {
-    $videos = Get-ChildItem "$location\*.mkv"
-    foreach ($video in $videos) {
-        ffmpeg -threads 2 -i "$video" -filter_complex "channelsplit=channel_layout=5.1[$letterMap01][$letterMap02][$letterMap03][$letterMap04][$letterMap05][$letterMap06]"	-map "[$letterMap01]" "$location\Converted\$($video.BaseName)___TRACK01.mp3" -map "[$letterMap02]" "$location\Converted\$($video.BaseName)___TRACK02.mp3" -map "[$letterMap03]" "$location\Converted\$($video.BaseName)___DIALOGUE.mp3" -map "[$letterMap04]" "$location\Converted\$($video.BaseName)___TRACK04.mp3" -map "[$letterMap05]" "$location\Converted\$($video.BaseName)___TRACK05.mp3" -map "[$letterMap06]" "$location\Converted\$($video.BaseName)___TRACK06.mp3"
-	    ffmpeg -threads 2 -i $video $dolby2 "$location\Converted\$($video.BaseName).mp4"
-	    ffmpeg -threads 2 -i $video -c:v libx264 -profile:v baseline -crf 16  -pix_fmt yuv420p -vf "$dolby,scale=640:360" -an "$location\Converted\Proxy\$($video.BaseName)_PROXY.mp4" 
-    }
-}
-
-Function Start-MakeScreencapsRestricted {
-    $videos = Get-ChildItem "$location\*.mkv"
-
-    foreach ($video in $videos) {
-        New-Item -Itemtype Directory "$location\Screencaps\$($video.BaseName)"
-        $outputDir = "$location\Screencaps\$($video.BaseName)"
-        ffmpeg -threads 8 -i "$video" -vf "$dolby,$data" -vsync 0 -q:v 6 "$outputDir\$($video.BaseName)_%03d.jpg"
-    }
-}
-Function Start-MakeScreencaps {
-    $videos = Get-ChildItem "$location\*.mkv"    
-    foreach ($video in $videos) {
-        $alreadyDone = Test-Path "$location\Screencaps\$($video.BaseName)"
-        if ($alreadyDone -eq $true) {
-            Continue
-        } else {
-            New-Item -Itemtype Directory "$location\Screencaps\$($video.BaseName)"
-            $outputDir = "$location\Screencaps\$($video.BaseName)"
-            ffmpeg -i "$video" -vf "$dolby,$data" -vsync 0 -q:v 6 "$outputDir\$($video.BaseName)_%03d.jpg"
-        }
-    }
-}
-
-
-
 Function Out-Script {
     Write-Host "Finishing up."
     $endingVars = Get-Variable
@@ -72,115 +5,123 @@ Function Out-Script {
     Exit
 }
 
+Function Start-MKVtoMP4 {
+    param (
+        [int]$threads,
+        [switch]$dolby,
+        [switch]$4kStandard,
+        [switch]$1080p,
+        [string]$location,        
+        [object]$video
+    )
+    New-Item -Itemtype Directory "$location\Converted"
+    New-Item -Itemtype Directory "$location\Converted\Proxy"
+
+    if ($threads -gt 0) {
+        $threadsSetting = "-threads $threads"
+    } else {
+        $threadsSetting = ""
+    }
+
+    if ($4kStandard) {
+        $mainSettings = "-c:a copy -c:v libx265 -crf 16 -color_primaries bt2020 -color_trc smpte2084 -colorspace bt2020nc -profile:v main10 -pix_fmt yuv420p10le"
+        $proxySettings = "-c:a copy -c:v libx265 -crf 30 -color_primaries bt2020 -color_trc smpte2084 -colorspace bt2020nc -profile:v baseline -pix_fmt yuv420p `"scale=640:360`""
+    } elseif ($dolby) {
+        $mainSettings = "c:a copy -c:v -c:v libx265 -crf 16 -vf `"hwupload,tonemap_opencl=tonemap=bt2390:desat=0:peak=100:format=nv12,hwdownload,format=nv12`""
+        $proxySettings = "-c:a copy -c:v libx265 -crf 30 -profile:v baseline -pix_fmt yuv420p10le -vf `"hwupload,tonemap_opencl=tonemap=bt2390:desat=0:peak=100:format=nv12,hwdownload,format=nv12,scale=640:360`" -an"
+    } elseif ($1080p) {
+        $mainSettings = "-c:a copy"
+        $proxySettings = "-c:a copy -c:v libx264 -profile:v baseline -crf 16  -pix_fmt yuv420p -vf `"scale=640:360`" -an" 
+    }
+
+    ffmpeg $threadsSetting -i "$video" $mainSettings "$location\Converted\Proxy\$($video.BaseName).mp4"
+    ffmpeg $threadsSetting -i "$video" $proxySettings "$location\Converted\Proxy\$($video.BaseName)_PROXY.mp4"
+}
+
+Function Start-ExtractAudio {
+    param (
+        [switch]$complex,
+        [switch]$basic,
+        [switch]$threads,
+        [array]$letterMapping,
+        [string]$location,
+        [object]$video
+    )
+
+    if ($complex) {
+        if ($letterMapping = 0) {
+            $letterMapping[0] = "L"
+            $letterMapping[1] = "R"
+            $letterMapping[2] = "C"
+            $letterMapping[3] = "LFE"
+            $letterMapping[4] = "Ls"
+            $letterMapping[5] = "Rs"
+        }
+
+        $audioMap = "-filter_complex `"channelsplit=channel_layout=5.1[${$letterMapping[0]}][${$letterMapping[1]}][${$letterMapping[2]}][${$letterMapping[3]}][${$letterMapping[4]}][${$letterMapping[5]}]`"	-map `"[${$letterMapping[0]}]`" `"$location\Converted\$($video.BaseName)___TRACK01.mp3`" -map `"[${$letterMapping[1]}]`" `"$location\Converted\$($video.BaseName)___TRACK02.mp3`" -map `"[${$letterMapping[2]}]`" `"$location\Converted\$($video.BaseName)___DIALOGUE.mp3`" -map `"[${$letterMapping[3]}]`" `"$location\Converted\$($video.BaseName)___TRACK04.mp3`" -map `"[${$letterMapping[4]}]`" `"$location\Converted\$($video.BaseName)___TRACK05.mp3`" -map `"[${$letterMapping[5]}]`" `"$location\Converted\$($video.BaseName)___TRACK06.mp3`""
+
+    } else {
+        $audioMap = "-map 0:a:0 `"$location\Converted\$($video.BaseName).mp3`" -map 0:a:1 `"$location\Converted\$($video.BaseName).mp3`" -map 0:a:2 `"$location\Converted\$($video.BaseName).mp3`" -map 0:a:3 `"$location\Converted\$($video.BaseName).mp3`" -map 0:a:4 `"$location\Converted\$($video.BaseName).mp3`" -map 0:a:5 `"$location\Converted\$($video.BaseName).mp3`""
+    }
+
+    if ($threads -gt 0) {
+        $threadsSetting = "-threads $threads"
+    } else {
+        $threadsSetting = ""
+    }
+
+    ffmpeg $threadsSetting -i "$video" $audioMap
+
+}
+
+Function Start-MakeScreencaps {
+    param (
+        [switch]$threads,
+        [switch]$dolby,
+        [switch]$4kStandard,
+        [switch]$1080p,
+        [string]$location,
+        [object]$video
+    )
+
+    if ($threads -gt 0) {
+        $threadsSetting = "-threads $threads"
+    } else {
+        $threadsSetting = ""
+    }
+
+    if ($dolby) {
+        $screenSettings = "-vf `"hwupload,tonemap_opencl=tonemap=bt2390:desat=0:peak=100:format=nv12,hwdownload,format=nv12,select='not(mod(n\,10))'`""
+    } elseif ($4kStandard) {
+        $screenSettings = "-color_primaries bt2020 -color_trc smpte2084 -colorspace bt2020nc -vf `"hwupload,tonemap_opencl=tonemap=bt2390:desat=0:peak=100:format=nv12,hwdownload,format=nv12,select='not(mod(n\,10))'`""
+    } elseif ($1080p) {
+        $screenSettings = "`"select='not(mod(n\,10))'`""
+    }
+
+    New-Item -Itemtype Directory "$location\Screencaps"
+    New-Item -Itemtype Directory "$location\Screencaps\$($video.BaseName)"
+
+    ffmpeg $threadsSetting -i "$video" $screenSettings -vsync 0 -q:v 6 "$location\Screencaps\$($video.BaseName)\$($video.BaseName)_%03d.jpg"
+}
+
+
+
+
+
+#FUNCTIONS ABOVE#
+
 $startingVars = Get-Variable
 
-$yesNoQuestion = "&Yes", "&No"
-$letterMappingsQuestion = "&Standard", "&Complex"
-$conCapQuestion = "Con&vert", "&Caps"
+#SCRIPT START#
 
-$location = Read-Host -prompt "Where are the files?"
+$video = Get-Item "H:\TV\House of the Dragon\Season 1\House.of.the.Dragon.S01E01.2160p.HMAX.WEB-DL.x265.10bit.HDR.DDP5.1.Atmos-SMURF.mkv"
 
-$restrictCPUChoice = $Host.UI.PromptForChoice("CPU", "Restrict CPU Threads?", $yesNoQuestion, 0)
+Start-MakeScreencaps -4kStandard -location "H:\TV\House of the Dragon\Season 1\" -video $video
 
-$conCapChoice = $Host.UI.PromptForChoice("Type", "Conversion or Caps?", $conCapQuestion, 0)
 
-$dummyChoice = $Host.UI.PromptForChoice("Dummy", "Run a dummy to get crop size?", $yesNoQuestion, 1)
 
-$cropChoice = $Host.UI.PromptForChoice("Crop?", "Does the video need cropping?", $yesNoQuestion, 1)
 
-$data = "select='not(mod(n\,10))'"
 
-$dolbyChoice = $Host.UI.PromptForChoice("Dolby?", "Is the video Dolby colored?", $yesNoQuestion, 1)
+#SCRIPT END#
 
-if ($dolbyChoice = 0) {
-    $dolby = "hwupload,tonemap_opencl=tonemap=bt2390:desat=0:peak=100:format=nv12,hwdownload,format=nv12"
-} else {
-    $dolby = ""
-}
-
-if ($cropChoice -eq 0) {
-    $crop = Read-Host -prompt "Crop"
-    if ($crop -eq "") {
-        $data2 = "crop=3840:1920:0:120,$data"
-        $data = $data2
-    } else {
-    $data2 = "$crop,$data"
-    $data = $data2
-    }
-}
-
-if ($dummyChoice -eq 0) {
-    $videos = Get-ChildItem -File $location
-    ffmpeg -ss 600 -i "$video[0]" -vf "cropdetect=24:16:0" "$location\dummy.mkv"
-}
-
-if ($conCapChoice -eq 0) {
-    New-Item -ItemType Directory "$location\Converted"
-    New-Item -ItemType Directory "$location\Converted\Proxy"
-    $letterMappingsChoice = $Host.UI.PromptForChoice("Audio Map", "What kind of Audio Mapping?", $letterMappingsQuestion, 0)
-    
-    if ($dolby -eq "") {
-        $dolby2 = ""
-    } else {
-        $dolby2 = "-vf `"$dolby`""
-    }
-
-    if ($letterMappingsChoice -eq 0) {
-        $letterMappings = "Standard"
-    }
-    if ($letterMappingsChoice -eq 1) {
-        $letterMappings = "Complex"
-        $letterMappingsDefault = $Host.UI.PromptForChoice("Letters", "Use the default letter mappings?", $yesNoQuestion, 0)
-        if ($letterMappingsDefault -eq 0)
-        {
-            $letterMap01 = "L"
-            $letterMap02 = "R"
-            $letterMap03 = "C"
-            $letterMap04 = "LFE"
-            $letterMap05 = "Ls"
-            $letterMap06 = "Rs"
-        }
-        if ($letterMappingsDefault -eq 1) {
-            $letterMap01 = Read-Host -prompt "Channel map letter 1?"
-            $letterMap02 = Read-Host -prompt "Channel map letter 2?"
-            $letterMap03 = Read-Host -prompt "Channel map letter 3?"
-            $letterMap04 = Read-Host -prompt "Channel map letter 4?"
-            $letterMap05 = Read-Host -prompt "Channel map letter 5?"
-            $letterMap06 = Read-Host -prompt "Channel map letter 6?"
-        }
-    }
-
-    Write-Host "Mappings are $letterMappings"
-
-}
-
-if ($typeChoice -eq 0) {
-    if ($letterMappings -eq "Standard" -and $restrictCPUChoice -eq 0) {
-        Start-RestrictedStandard
-        Out-Script
-    }
-    if ($letterMappings -eq "Complex" -and $restrictCPUChoice -eq 0) {
-        Start-RestrictedComplex
-        Out-Script
-    }
-    if ($letterMappings -eq "Standard" -and $restrictCPUChoice -eq 1) {
-        Start-UnlimitedStandard
-        Out-Script
-    }
-    if ($letterMappings -eq "Complex" -and $restrictCPUChoice -eq 1) {
-        Start-UnlimitedComplex
-        Out-Script
-    }
-} 
-
-if ($conCapChoice -eq 1) {
-    if ($restrictCPUChoice -eq 0)
-    {
-        Start-MakeScreencapsRestricted
-        Out-Script
-    }
-    if ($restrictCPUChoice -eq 1) {
-        Start-MakeScreencaps
-        Out-Script
-    }
-}
+Out-Script
